0e2b80259c0d48899ba9ba35e2511b4267e09915
Egor14 <egorgrekhnev@mail.ru>
2019-04-29 01:04:09 +0300

    connection with web

--- bot.py
+++ bot.py
@@ -9,7 +9,9 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Пришли мне фотографию и жди результата!")
+    bot.reply_to(message, "Напиши мне номер модели, и ты получишь всю необходимую информацию\n" +
+                 "Если же ты хочешь добавить свою технику в нашу базу для быстрого поиска клиентов, то сделать это можно по ссылке: " +
+                 "https://beta-equipment.herokuapp.com")
 
 
 @bot.message_handler(func=lambda message: True)
