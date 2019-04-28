423eae3afad0cded0a21be164657c24110db5126
Egor14 <egorgrekhnev@mail.ru>
2019-04-28 21:33:09 +0300

    checking fix

--- bot.py
+++ bot.py
@@ -14,19 +14,20 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    print(message.text)
+    #print(message.text)
     conn = psycopg2.connect(dbname='dfttd1019a6clc', user='pmjdivbrbpkefa',
                             password='0d2581df34fa354b9f6b2b1d0a613e63404d501ee39b2a6fcf3b47e4d0c126ce',
                             host='ec2-54-225-129-101.compute-1.amazonaws.com')
-    print(message.text)
+    #print(message.text)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM home_car')
     records = cursor.fetchall()
-    print(records)
+    bot.reply_to(message, 'Egor')
+    #print(records)
     cursor.close()
     conn.close()
 
-    print(message.text)
+    #print(message.text)
     bot.reply_to(message, 'Такой модели нет в наличии')
 
 
